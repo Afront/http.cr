837cb9bf7249625c98e58aded3b6709ad79ba2a8
Afront <3943720+Afront@users.noreply.github.com>
2020-04-18 13:39:03 +0900

    Create basic 'Hello, World!' server

--- 2019313715.py
+++ 2019313715.py
@@ -1,9 +1,17 @@
 from socket import *
 from datetime import datetime
 import logging
+import signal
+import sys
 
 BUFFER_SIZE = 2048
 MAX = 100
+
+def handler(signal_received, frame):
+	print("Goodbye!")
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
 
 logger = logging.getLogger("logger")
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(name)s::[%(levelname)s]: %(message)s')
@@ -14,19 +22,22 @@ server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
 server_socket.listen(1)
 
-number = randint(0, MAX)
-guesses = 0
-
 logging.info('Server is ready!')
-conn, clientAddress = server_socket.accept()
 
 while True:
+	conn, clientAddress = server_socket.accept()
 	request = conn.recv(BUFFER_SIZE)	
 
 	if not request:
+		conn.close()
 		break
 	else:
+		print(request)
 		decoded_request = request.decode()
 		logging.info(f"{datetime.now()}: {clientAddress} - {decoded_request}")
 
+		response = "HTTP/1.0 200 OK\n\nHello World!"
 		conn.send(response.encode())
+#		conn.close()
+
+server_socket.close()
